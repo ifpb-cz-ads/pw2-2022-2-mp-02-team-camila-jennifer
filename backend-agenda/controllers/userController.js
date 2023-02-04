4c87e8d0fb54f80b2afeb01866e5d1f03dbdb4b8
JenniferSilva46 <jennifer.silva@academico.ifpb.edu.br>
2023-02-03 22:33:34 -0300

    Mudando remetente

--- backend-agenda/controllers/userController.js
+++ backend-agenda/controllers/userController.js
@@ -11,7 +11,7 @@ const validateCodeUser = (email) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const msg = {
-    from: 'camila.casimiro@academico.ifpb.edu.br',
+    from: 'silvajennifer2509@gmail.com',
     to: email,
     subject: 'Confirmação de Email',
     text: 'Confirmação de Email',
